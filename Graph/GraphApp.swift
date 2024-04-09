b7aa495f8b81625551135b9c3a42b11cdcb4226b
Philippe MICHEL <philippe.michel1@gmail.com>
2024-04-09 11:53:08 +0200

    Initial Commit

---
+++ Graph/GraphApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraphApp.swift
+//  Graph
+//
+//  Created by Philippe MICHEL on 09/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct GraphApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
