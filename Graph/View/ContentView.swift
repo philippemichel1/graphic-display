4bcbe779032e7cb5badfa3d475a789b2c93d9e5b
Philippe MICHEL <philippe.michel1@gmail.com>
2024-04-11 19:14:25 +0200

    animation

--- Graph/View/ContentView.swift
+++ Graph/View/ContentView.swift
@@ -10,12 +10,13 @@ import SwiftUI
 struct ContentView: View {
     // changer la taille de la police dans le navigationTitle
     init() {
-           UINavigationBar.appearance().largeTitleTextAttributes = [.font : UIFont.preferredFont(forTextStyle: .footnote)]
-           UINavigationBar.appearance().titleTextAttributes = [.font : UIFont.preferredFont(forTextStyle: .footnote)]
-       }
+        UINavigationBar.appearance().largeTitleTextAttributes = [.font : UIFont.preferredFont(forTextStyle: .footnote)]
+        UINavigationBar.appearance().titleTextAttributes = [.font : UIFont.preferredFont(forTextStyle: .footnote)]
+    }
     
     @State private var textToShow = ""
     @State private var currentIndex = 0
+    @State private var endOfSentance:Bool = false
     let fullText = "Insérer facilement des graphiques dans vos applications."
     
     var connectionmusee:APIConnection = APIConnection()
@@ -28,85 +29,91 @@ struct ContentView: View {
     var body: some View {
         NavigationStack{
             VStack {
-//                Text(textToShow)
-//                    .font(.headline)
-//                    .padding()
-//                    .foregroundColor(Color.black)
-//                    .frame(width: 350, height: 150)
+                Text(textToShow)
+                    .font(.headline)
+                    .padding()
+                    .foregroundColor(.accentColor)
+                    .frame(width: 350, height: 150)
                 
                 HStack(spacing: 7){
-                    Button(action: {
-                        getNumberPress(numberButton: 0)
-                        
-                    }, label: {
-                        Text("Entrées Payantes")
-                            .multilineTextAlignment(.center)
-                    })
-                    .frame(width: buttonWidth, height: buttonHeight)
-                    .background(.red)
-                    .foregroundColor(.white)
-                    .cornerRadius(buttonCorner)
-                    .buttonStyle(BorderedButtonStyle())
-                    Button(action: {
-                        getNumberPress(numberButton: 1)
-                      
-                        
-                    }, label: {
-                        Text("Entrées Gratuites")
-                            .multilineTextAlignment(.center)
-                    })
-                    .frame(width: buttonWidth, height: buttonHeight)
-                    .background(.green)
-                    .foregroundColor(.white)
-                    .cornerRadius(buttonCorner)
-                    .buttonStyle(BorderedButtonStyle())
-                    Button(action: {
-                        getNumberPress(numberButton: 2)
-                    }, label: {
-                        Text("Total entrées")
-                            .multilineTextAlignment(.center)
-                    })
-                    .frame(width: buttonWidth, height: buttonHeight)
-                    .background(.purple)
-                    .foregroundColor(.white)
-                    .cornerRadius(buttonCorner)
-                    .buttonStyle(BorderedButtonStyle())
+                    if endOfSentance {
+                        Button(action: {
+                            getNumberPress(numberButton: 0)
+                            
+                        }, label: {
+                            Text("Entrées Payantes")
+                                .multilineTextAlignment(.center)
+                        })
+                        .frame(width: buttonWidth, height: buttonHeight)
+                        .background(.red)
+                        .foregroundColor(.white)
+                        .cornerRadius(buttonCorner)
+                        .buttonStyle(BorderedButtonStyle())
+                        Button(action: {
+                            getNumberPress(numberButton: 1)
+                            
+                            
+                        }, label: {
+                            Text("Entrées Gratuites")
+                                .multilineTextAlignment(.center)
+                        })
+                        .frame(width: buttonWidth, height: buttonHeight)
+                        .background(.green)
+                        .foregroundColor(.white)
+                        .cornerRadius(buttonCorner)
+                        .buttonStyle(BorderedButtonStyle())
+                        Button(action: {
+                            getNumberPress(numberButton: 2)
+                        }, label: {
+                            Text("Total entrées")
+                                .multilineTextAlignment(.center)
+                        })
+                        .frame(width: buttonWidth, height: buttonHeight)
+                        .background(.purple)
+                        .foregroundColor(.white)
+                        .cornerRadius(buttonCorner)
+                        .buttonStyle(BorderedButtonStyle())
+                    }
                 }
                 .fullScreenCover(isPresented: $showViewGraph) {
                     switch buttonPressed {
                     case 0:
-                      PaidTickets()
+                        PaidTickets()
                     case 1:
                         FreeTickets()
                     default:
                         TotalTickets()
-                                            
+                        
                     }
                 }
             }
-            .navigationTitle("Fréquentation Musée")
+            .navigationTitle("Fréquentation des Musée")
             
         }
         .onAppear {
-            //DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-               // showNextLetter()
-           // }
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                showNextLetter()
+            }
         }
     }
-    // obtenir le bouron qui est appuyé 
+    // obtenir le bouron qui est appuyé
     func getNumberPress(numberButton:Int) {
         buttonPressed = numberButton
-        print("numero Bouton: \(buttonPressed)")
+        print("numero Bouton: \(buttonPressed!)")
         self.showViewGraph = true
     }
     // animation sur le texte
     func showNextLetter() {
-        guard currentIndex < fullText.count else { return }
-        textToShow += String(fullText[fullText.index(fullText.startIndex, offsetBy: currentIndex)])
-        currentIndex += 1
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
-            showNextLetter()
+        // guard currentIndex < fullText.count else { return }
+        if currentIndex < fullText.count {
+            textToShow += String(fullText[fullText.index(fullText.startIndex, offsetBy: currentIndex)])
+            currentIndex += 1
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                showNextLetter()
+            }
+        } else {
+            endOfSentance = true
         }
     }
 }
